6a2b3e9d66ba4965550db1f196f29af6e3a07f59
coolbeatz71 <sigmacool@gmail.com>
2025-05-06 01:16:50 +0200

    chore: add more service extensions

--- src/Modules/Ordering/Ordering/Ordering.cs
+++ src/Modules/Ordering/Ordering/Ordering.cs
@@ -1,11 +1,11 @@
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 
-namespace EShop.Catalog;
+namespace EShop.Ordering;
 
-public static class CatalogModule
+public static class OrderingModule
 {
-    public static IServiceCollection AddCatalogModule(this IServiceCollection services, IConfiguration configuration)
+    public static IServiceCollection AddOrderingModule(this IServiceCollection services, IConfiguration configuration)
     {
         // Add services to the container.
         // services
@@ -15,6 +15,7 @@ public static class CatalogModule
 
         return services;
     }
+
 }
 
 
