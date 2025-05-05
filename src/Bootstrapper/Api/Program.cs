556840bd139cf075699bf41d38361b033d5bc360
coolbeatz71 <sigmacool@gmail.com>
2025-05-05 23:40:07 +0200

    feat: DI:  add CatalogModule service

--- src/Bootstrapper/Api/Program.cs
+++ src/Bootstrapper/Api/Program.cs
@@ -1,7 +1,10 @@
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-
+builder.Services
+    .AddCatalogModule(builder.Configuration)
+    .AddBasketModule(builder.Configuration)
+    .AddIdentityModule(builder.Configuration);
 var app = builder.Build();
 
 app.MapGet("/", () => "Hello World!!");
