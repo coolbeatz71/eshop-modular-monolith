b963fd77c265343f6a2a0bf0a42168018dddc0d8
coolbeatz71 <sigmacool@gmail.com>
2025-05-05 23:40:07 +0200

    feat: DI:  add CatalogModule service

---
+++ src/Modules/Catalog/Catalog/CatalogModule.cs
@@ -0,0 +1,21 @@
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace EShop.Catalog;
+
+public static class CatalogModule
+{
+    public static IServiceCollection AddCatalogModule(this IServiceCollection services, IConfiguration configuration)
+    {
+        // Add services to the container.
+        // services
+        //     .AddApplicationServices().
+        //     AddInfrastructureServices()
+        //     .AddApiServices();
+
+        return services;
+    }
+
+}
+
+
