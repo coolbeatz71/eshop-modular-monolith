cec92fe13b95824a15524fafc3edad0116abc916
coolbeatz71 <sigmacool@gmail.com>
2025-05-06 20:01:53 +0200

    [chore] create shared root aggregate

---
+++ src/Shared/Shared/Domain/IAggregate.cs
@@ -0,0 +1,11 @@
+namespace EShop.Shared.Domain;
+
+public interface IAggregate<T> : IAggregate, IEntity<T>
+{
+}
+
+public interface IAggregate: IEntity
+{
+    IReadOnlyList<IDomainEvent> DomainEvents { get; }
+    IDomainEvent[] ClearDomainEvents();
+}
