4ca412a1c3a3e444903f510fa2ee4830dcb6dc11
coolbeatz71 <sigmacool@gmail.com>
2025-05-09 10:55:58 +0200

    chore: setup CatalogDbContext and EntityTypeConfiguration

---
+++ src/Modules/Catalog/Catalog/DataSource/Configurations/ProductConfiguration.cs
@@ -0,0 +1,18 @@
+using EShop.Catalog.Products.Models;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace EShop.Catalog.DataSource.Configurations;
+
+public class ProductConfiguration: IEntityTypeConfiguration<Product>
+{
+    public void Configure(EntityTypeBuilder<Product> builder)
+    {
+        builder.HasKey(p => p.Id);
+        builder.Property(p => p.Name).HasMaxLength(50).IsRequired();
+        builder.Property(p => p.Category).IsRequired();
+        builder.Property(p => p.Description).HasMaxLength(200).IsRequired();
+        builder.Property(p => p.ImageFile).HasMaxLength(100);
+        builder.Property(p => p.Price).HasColumnType("decimal(18,2)").IsRequired();
+    }
+}
