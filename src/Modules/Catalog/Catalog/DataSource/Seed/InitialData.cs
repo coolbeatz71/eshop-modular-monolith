b023b89a874d1c1c2c1022da0ed301cc1cc1cb27
coolbeatz71 <sigmacool@gmail.com>
2025-05-14 12:02:58 +0200

    chore: use Bogus .NET to generate fake products

--- src/Modules/Catalog/Catalog/DataSource/Seed/InitialData.cs
+++ src/Modules/Catalog/Catalog/DataSource/Seed/InitialData.cs
@@ -1,6 +1,22 @@
-namespace EShop.Catalog.DataSource.Seed;
+using Bogus;
+using EShop.Catalog.Products.Models;
 
-public static class InitialData
+namespace EShop.Catalog.DataSource.Seed
 {
-    
+    public static class InitialData
+    {
+        private static readonly Faker Faker = new Faker();
+
+        public static IEnumerable<Product> Products =>
+            Enumerable.Range(1, 5).Select(_ =>
+                Product.Create(
+                    Guid.NewGuid(),
+                    Faker.Commerce.ProductName(),
+                    Faker.Commerce.ProductDescription(),
+                    Faker.Image.PicsumUrl(),
+                    decimal.Parse(Faker.Commerce.Price()),
+                    Faker.Commerce.Categories(1).ToList()
+                )
+            );
+    }
 }
