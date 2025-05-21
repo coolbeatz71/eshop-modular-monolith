fcb6b022ab15befb94ee1868e7e326f06cdde75c
coolbeatz71 <sigmacool@gmail.com>
2025-05-21 21:24:52 +0200

    chore: create shared routemetadata
    should contain all the info needed to document endpoints

---
+++ src/Modules/Catalog/Catalog/Products/UseCases/CreateProduct/RouteMetadata.cs
@@ -0,0 +1,20 @@
+using EShop.Shared.Metadata;
+
+namespace EShop.Catalog.Products.UseCases.CreateProduct;
+
+public static class ProductRoutes
+{
+    public static readonly RouteMetadata CreateProductRouteMetadata = new(
+        name: "CreateProduct",
+        summary: "Register a new product in the catalog.",
+        description: """
+             Accepts product details as input and creates a new product entry in the system.
+
+             On success, returns a 201 Created response along with the newly created product's data,
+             including its unique identifier.
+
+             If the request is invalid or fails validation, a 400 Bad Request response is returned.
+        """
+    );
+}
+
