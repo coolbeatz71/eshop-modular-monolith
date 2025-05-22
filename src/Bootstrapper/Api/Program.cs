dacfc08d8fd19fc2e5351831c4612328b8ad842c
coolbeatz71 <sigmacool@gmail.com>
2025-05-22 21:34:29 +0200

    chore: create extension method for carter assembly registrations

--- src/Bootstrapper/Api/Program.cs
+++ src/Bootstrapper/Api/Program.cs
@@ -1,4 +1,5 @@
 using Carter;
+using EShop.Shared.Extensions;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -7,13 +8,8 @@ DotNetEnv.Env.Load();
 DotNetEnv.Env.TraversePath().Load();
 
 // Add services to the container.
-builder.Services.AddCarter(configurator: (config) =>
-{
-    var catalogModules = typeof(CatalogModule).Assembly.GetTypes()
-        .Where(t => t.IsAssignableTo(typeof(ICarterModule))).ToArray();
-    
-    config.WithModules(catalogModules);
-});
+// Register Carter Assembly
+builder.Services.AddCarterWithAssemblies(typeof(CatalogModule).Assembly);
 
 builder.Services
     .AddCatalogModule(builder.Configuration)
