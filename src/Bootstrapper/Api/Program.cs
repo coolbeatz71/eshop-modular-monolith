913be947f480b6a5f6900c9dc425f3b3bc20229b
coolbeatz71 <sigmacool@gmail.com>
2025-05-22 21:17:29 +0200

    chore: register carter library, add catalog module

--- src/Bootstrapper/Api/Program.cs
+++ src/Bootstrapper/Api/Program.cs
@@ -1,3 +1,5 @@
+using Carter;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Load environments variables from .env file
@@ -5,13 +7,21 @@ DotNetEnv.Env.Load();
 DotNetEnv.Env.TraversePath().Load();
 
 // Add services to the container.
+builder.Services.AddCarter(configurator: (config) =>
+{
+    var catalogModules = typeof(CatalogModule).Assembly.GetTypes()
+        .Where(t => t.IsAssignableTo(typeof(ICarterModule))).ToArray();
+    
+    config.WithModules(catalogModules);
+});
+
 builder.Services
     .AddCatalogModule(builder.Configuration)
     .AddBasketModule(builder.Configuration)
     .AddOrderingModule(builder.Configuration);
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello World!!");
+app.MapCarter();
 
 // Configure middleware extensions for catalog, basket and ordering modules.
 app
