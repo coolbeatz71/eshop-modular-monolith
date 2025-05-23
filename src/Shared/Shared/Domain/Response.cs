63dea9d303af129dca0d328ca682f0f36e202731
coolbeatz71 <sigmacool@gmail.com>
2025-05-24 01:36:01 +0200

    chore: create shared API response blueprint

--- src/Shared/Shared/Domain/Response.cs
+++ src/Shared/Shared/Domain/Response.cs
@@ -1,6 +1,18 @@
 namespace EShop.Shared.Domain;
 
-public class Response
+public class Response<TResult>
 {
-    
+    public bool IsSuccess { get; }
+    public string? Error { get; }
+    public TResult? Value { get; }
+
+    private Response(bool isSuccess, TResult? value, string? error)
+    {
+        IsSuccess = isSuccess;
+        Value = value;
+        Error = error;
+    }
+
+    public static Response<TResult> Success(TResult value) => new(true, value, null);
+    public static Response<TResult> Failure(string error) => new(false, default, error);
 }
--- src/Shared/Shared/Extensions/ResponseExtension.cs
+++ src/Shared/Shared/Extensions/ResponseExtension.cs
@@ -1,6 +1,14 @@
+using EShop.Shared.Domain;
+using Microsoft.AspNetCore.Http;
+
 namespace EShop.Shared.Extensions;
 
-public class ResponseExtension
+public static class ResponseExtension
 {
-    
+    public static IResult ToApiResult<T>(this Response<T> result, Func<T, IResult> onSuccess)
+    {
+        return result.IsSuccess
+            ? onSuccess(result.Value!)
+            : Results.BadRequest(new { error = result.Error });
+    }
 }
