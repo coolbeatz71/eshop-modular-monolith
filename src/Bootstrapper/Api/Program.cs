477f3342a9d1ae61a6daddc9a7c39f5ff6738d3d
coolbeatz71 <sigmacool@gmail.com>
2025-06-20 17:05:58 +0200

    chore: register basket service to Carter assemblies

--- src/Bootstrapper/Api/Program.cs
+++ src/Bootstrapper/Api/Program.cs
@@ -13,8 +13,13 @@ DotNetEnv.Env.Load();
 DotNetEnv.Env.TraversePath().Load();
 
 // Add services to the container.
-// Register Carter Assembly
-builder.Services.AddCarterWithAssemblies(typeof(CatalogModule).Assembly);
+// Register Carter Assemblies
+var catalogAssembly = typeof(CatalogModule).Assembly;
+var basketAssembly = typeof(BasketModule).Assembly;
+builder.Services.AddCarterWithAssemblies(
+    catalogAssembly, 
+    basketAssembly
+);
 
 builder.Services
     .AddCatalogModule(builder.Configuration)
